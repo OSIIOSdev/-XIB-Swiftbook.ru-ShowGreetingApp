481c7b3d31bda38f38f216298b76bf05472f0ca1
OSIIOSdev <OSIIOS.dev@gmail.com>
2025-03-06 16:20:31 -0800

    Initial Commit

---
+++ ShowGreetingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShowGreetingApp
+//
+//  Created by OSIIOSdev on 06.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
